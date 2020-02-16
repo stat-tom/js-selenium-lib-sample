32b2b913f88e3533ad187f4fe0768a0f6524106f
hunrin <t84sta@gmail.com>
2020-02-16 20:45:20 +0100

    Change index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const {Builder, By, Key, util} = require("selenium-webdriver");
 
-async function example() {
-    let driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("https://tsjs.pl/testowanie");
-    await driver.findElement(By.name("q")).sendKeys("testowanie", Key.RETURN);
-}
+// async function example() {
+//     let driver = await new Builder().forBrowser("chrome").build();
+//     await driver.get("http://google.pl");
+//     await driver.findElement(By.name("q")).sendKeys("tsjs.pl", Key.RETURN);
+// }
 
-example();
+// example();
+
+const driver = new Builder().forBrowser("chrome").build();
+
+driver.get("https://tsjs.pl");
