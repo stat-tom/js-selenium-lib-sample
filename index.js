fdf0b49cbbb59c42e52f76c272cb38235ff694d6
hunrin <t84sta@gmail.com>
2020-02-27 21:24:30 +0100

    Change index.js by adding promises

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-const { Builder, By, Key, util } = require("selenium-webdriver");
-
-const firefox = require("selenium-webdriver/firefox");
-const proxy = require("selenium-webdriver/proxy");
-
-const options = new firefox.Options();
-options.setProfile("./SeleniumTutorial");
-const proxyServer = "182.253.93.2:53281";
-
-// options.setPreference("browser.download.dir", "C:\\mySeleniumDownloads");
-// options.setPreference("browser.download.folderList", 2);
-// options.setPreference("browser.helperApps.neverAsk.saveToDisk", "applications/x-csv");
-
-// async function example() {
-//     let driver = await new Builder().forBrowser("chrome").build();
-//     await driver.get("http://google.pl");
-//     await driver.findElement(By.name("q")).sendKeys("tsjs.pl", Key.RETURN);
-// }
-
-// example();
+const { Builder } = require("selenium-webdriver");
 
 const driver = new Builder()
   .forBrowser("firefox")
-  .setFirefoxOptions(options)
-  .setProxy(proxy.manual({
-      http: proxyServer,
-      https: proxyServer
-  }))
   .build();
 
-driver.get("http://google.com/");
+async function openWebsites() {
+    try {
+        await (await driver).get("https://www.google.pl/");
+        await (await driver).get("https://www.google.com/");   
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+openWebsites();
