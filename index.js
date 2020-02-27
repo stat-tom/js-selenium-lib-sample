cf1939e8ab80fc214f21040ecf1fa0697b62d387
hunrin <t84sta@gmail.com>
2020-02-27 20:16:33 +0100

    Change index.js by adding browser profile

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const {Builder, By, Key, util} = require("selenium-webdriver");
 
 const firefox = require("selenium-webdriver/firefox");
 const options = new firefox.Options();
-options.setPreference("browser.download.dir", "C:\\mySeleniumDownloads");
-options.setPreference("browser.download.folderList", 2);
-options.setPreference("browser.helperApps.neverAsk.saveToDisk", "applications/x-csv");
+
+options.setProfile("./SeleniumTutorial");
+
+// options.setPreference("browser.download.dir", "C:\\mySeleniumDownloads");
+// options.setPreference("browser.download.folderList", 2);
+// options.setPreference("browser.helperApps.neverAsk.saveToDisk", "applications/x-csv");
 
 
 // async function example() {
@@ -17,4 +20,4 @@ options.setPreference("browser.helperApps.neverAsk.saveToDisk", "applications/x-
 
 const driver = new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
 
-driver.get("http://insight.dev.schoolwires.com/HelpAssets/C2Assets/C2Files/C2ImportCalEventSample.csv");
+driver.get("http://google.com");
