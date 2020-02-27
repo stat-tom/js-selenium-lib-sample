ff5cf2579b13f7b212161c15f8d9b3b52873d424
hunrin <t84sta@gmail.com>
2020-02-27 23:00:32 +0100

    Change folder name

--- registrationForm/index.js
+++ registrationForm/index.js
@@ -1,10 +1,10 @@
-const { Builder, By } = require("./node_modules/selenium-webdriver");
+const { Builder, By } = require("selenium-webdriver");
 
 const driver = new Builder()
   .forBrowser("firefox")
   .build();
 
-async function checkRegistration() {
+async function registrationForm() {
     try {
         await driver.get("https://rori4.github.io/selenium-practice/#/pages/practice/simple-registration");
         await driver.findElement(By.name("email")).sendKeys("secretmail@gmail.com");   
@@ -15,4 +15,4 @@ async function checkRegistration() {
     }
 }
 
-checkRegistration();
+registrationForm();
