e4771beba670b1c4e4857ace5f45c1b4525d1383
hunrin <t84sta@gmail.com>
2020-02-29 15:29:20 +0100

    Checkbox form test

---
+++ checkboxForm/index.js
@@ -0,0 +1,22 @@
+const { Builder, By } = require("./node_modules/selenium-webdriver");
+
+const driver = new Builder()
+  .forBrowser("firefox")
+  .build();
+
+async function checkboxForm() {
+    try {
+        await driver.get("https://rori4.github.io/selenium-practice/#/pages/practice/checkbox-form");
+        await driver.findElement(By.xpath("//input[@id='name']")).sendKeys("Gimli");   
+        await driver.findElement(By.xpath("//input[@id='comment']")).sendKeys("Balin, Dwalin, Kili, Fili...");   
+        await driver.findElement(By.xpath("//input[@value='presidential-suite']/following::span")).click();   
+        await driver.findElement(By.xpath("//nb-checkbox[@value='breakfast']/label/span")).click();   
+        await driver.findElement(By.xpath("//nb-checkbox[@value='lunch']/label/span")).click();   
+        await driver.findElement(By.xpath("//button[@id='submit']")).click();   
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+checkboxForm();
+
