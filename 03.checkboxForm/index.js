2e789d5dfcc2bff4c25eaf9e237f113d93f60f1d
Tomasz Statkowski <43075475+stat-tom@users.noreply.github.com>
2021-08-17 10:15:48 +0200

    Update index.js

--- 03.checkboxForm/index.js
+++ 03.checkboxForm/index.js
@@ -7,8 +7,8 @@ const driver = new Builder()
 async function checkboxForm() {
     try {
         await driver.get("https://rori4.github.io/selenium-practice/#/pages/practice/checkbox-form");
-        await driver.findElement(By.xpath("//input[@id='name']")).sendKeys("Gimli");   
-        await driver.findElement(By.xpath("//input[@id='comment']")).sendKeys("Balin, Dwalin, Kili, Fili...");   
+        await driver.findElement(By.xpath("//input[@id='name']")).sendKeys("Syrena");   
+        await driver.findElement(By.xpath("//input[@id='comment']")).sendKeys("Fiat, Syrena, Polonez...");   
         await driver.findElement(By.xpath("//input[@value='presidential-suite']/following::span")).click();   
         await driver.findElement(By.xpath("//nb-checkbox[@value='breakfast']/label/span")).click();   
         await driver.findElement(By.xpath("//nb-checkbox[@value='lunch']/label/span")).click();   
